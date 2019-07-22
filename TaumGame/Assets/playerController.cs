fe751ff48f244495fe95f0c68fec8736478a9fd1
Dutch <carnplaynl@gmail.com>
2019-07-23 00:41:58 +0200

    Added some fancy animations and did the code for them

--- TaumGame/Assets/playerController.cs
+++ TaumGame/Assets/playerController.cs
@@ -9,16 +9,34 @@ public class playerController : MonoBehaviour
 
     private inputManager inputManager;
     private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         inputManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<inputManager>();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
         transform.LookAt(GameObject.FindGameObjectWithTag("MainCamera").transform);
+
+        if (inputManager.movementDirection.x != 0 || inputManager.movementDirection.y != 0)
+        {
+            animator.SetBool("Sideways", true);
+            animator.SetBool("Running", true);
+
+            if (inputManager.movementDirection.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else if (inputManager.movementDirection.x < 0) { spriteRenderer.flipX = true; }
+
+        } else { animator.SetBool("Running", false); }
+
     }
 
     void FixedUpdate()
